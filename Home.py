e73d15b50304a48039d1692d8b488f60b2d7c644
ajay <ajaykrish.krishnanrb@gmail.com>
2023-09-23 16:35:02 +0530

    first commit

---
+++ Home.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+from utils import caption_block, get_sentiment, spell_check, get_sentence_sentiment, sentiment_sample_text
+
+
+st.title('📝 Natural language processing')
+st.caption(caption_block)
+
+
+tab1, tab2 = st.tabs(["Sentiment Analysis", "Spelling Correction"])
+
+
+with tab1:
+    st.header("Sentiment Analysis")
+    sentiment_text = st.text_area(
+        label="Enter the text to analyse 🔍", value=sentiment_sample_text.strip())
+    sentiment_output = get_sentiment(text=sentiment_text)
+    st.metric(
+        label="$$Sentiment Score$$",
+        value=sentiment_output[0],
+        delta=round(sentiment_output[1], 2)
+    )
+
+    input_sentences = get_sentence_sentiment(sentiment_text)
+    for i_sen in input_sentences:
+        st.markdown(i_sen)
+
+
+with tab2:
+    st.header("Spelling Correction")
+
+    spelling_text = st.text_input(
+        label="Enter the text to spell check 🖋", value="I hav a car")
+    spelling_output = spell_check(spelling_text)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("#### Correction ✅")
+        st.write(spelling_output)
+    with col2:
+        st.markdown("#### Input ✍")
+        st.caption(spelling_text)
---
+++ utils.py
@@ -0,0 +1,61 @@
+from textblob import TextBlob
+import subprocess
+
+subprocess.run(['python3', '-m', 'textblob.download_corpora'])
+
+
+caption_block = """
+
+Natural language processing (NLP) refers to the branch of computer science and more specifically, the branch of artificial intelligence or AI concerned with giving computers the ability to understand text and spoken words in much the same way human beings can.
+
+"""
+
+
+sentiment_sample_text = '''
+McLaren's Lando Norris : I was pretty happy with my laps. It's a tricky circuit; not easy to put everything together but it's so quick around here and the smallest mistake can take a big amount of lap time. I'm happy. It's been a good day and good positions for tomorrow.
+'''
+
+
+def get_sentiment(text: str, threshold: float = 0.3):
+    '''
+    This function will output the sentiment
+    '''
+    blob = TextBlob(text)
+    sentiment: float = blob.sentiment.polarity
+    friendly_threshold: float = threshold
+    hostile_threshold: float = -threshold
+
+    if sentiment >= friendly_threshold:
+        return ('😍😃😊🥰', sentiment)
+
+    elif sentiment <= hostile_threshold:
+        return ('😢😡😭😞', sentiment)
+    else:
+        return ('😐😑😒😕', sentiment)
+
+
+def get_sentence_sentiment(text: str, threshold: float = 0.3):
+    '''
+    This function split the input into sentences and compute the sentiment score 😀
+    '''
+    blob = TextBlob(text)
+    result = []
+    for sen in blob.sentences:
+        res = ''
+        p = sen.sentiment.polarity
+        if p > threshold:
+            res = '😍'
+        elif p < -threshold:
+            res = "😭"
+        else:
+            res = '😐'
+        # concat
+        res = res + " : " + str(sen)
+        result.append(res)
+
+    return result
+
+
+def spell_check(text: str):
+    blob = TextBlob(text)
+    return blob.correct()
